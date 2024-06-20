25f6f8283b423d4e8186e3018e45c12553caf0ca
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-06-20 16:21:34 +0200

    Update command for pl.dtsi generation

--- platform/dtsi_gen.tcl
+++ platform/dtsi_gen.tcl
@@ -1,6 +1,1 @@
-hsi open_hw_design /home/francesco/Documents/mmr-kria-os/platform/kria_hw.xsa
-hsi set_repo_path /home/francesco/Documents/mmr-kria-os/layers/meta-kria/recipes-firmware/device-tree-xlnx/
-hsi create_sw_design device-tree -os device_tree -proc psu_cortexa53_0
-hsi set_property CONFIG.dt_overlay true [hsi::get_os]
-# hsi set_property CONFIG.dt_zocl true [hsi get_os]
-hsi generate_target -dir /home/francesco/Documents/mmr-kria-os/layers/meta-kria/recipes-firmware/mmr-firmware/mmr-firmware.dtsi
+createdts -hw /home/francesco/Documents/mmr-kria-os/platform/platform.xsa -platform-name kr260 -git-branch xlnx_rel_v2022.2 -compile -out /home/francesco/Documents/mmr-kria-os/platform/test
