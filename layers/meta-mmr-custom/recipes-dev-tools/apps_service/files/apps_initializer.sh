435d680985217e36103fe18f98fcc6ee2b38e9a6
Edoardo Torrini <edoardo.torrini@gmail.com>
2024-08-29 08:04:11 +0200

    modify apps_initializer

--- layers/meta-mmr-custom/recipes-dev-tools/apps_service/files/apps_initializer.sh
+++ layers/meta-mmr-custom/recipes-dev-tools/apps_service/files/apps_initializer.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/usr/bin/apps_initializer &
+while true
+do
+    A=$(pgrep control_node)
+    if [ -n "$A" ]; then
+        break
+    else
+        bash -c "/usr/bin/apps_initializer" > /tmp/log.txt
+    fi
+done
