85e34f11fd63e46993b0804c9c6c6c5b7de76d06
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2025-02-23 19:40:13 +0100

    Kickstart visualization scripts

---
+++ useful-scripts/scheduling-viz/jobs_sched_slots.py
@@ -0,0 +1,11 @@
+"""
+Plots jobs scheduled slot [arrival time, arrival time + exec time] on a single CPU
+"""
+
+import seaborn as sb
+import pandas as pd
+import matplotlib.pyplot as pl
+
+
+def main():
+    pass
